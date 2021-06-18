e2df1f34f52635949c5f8cd977cbd0fbe2bdbdcb
Michael Lamarre <michael_lamarre@comcast.com>
2021-06-18 16:19:26 -0700

    Add solution for problem 1700

--- src/test/scala/org/mlamarre/leetcode/LeetcodeSolutions.scala
+++ src/test/scala/org/mlamarre/leetcode/LeetcodeSolutions.scala
@@ -2,20 +2,14 @@ package org.mlamarre.leetcode
 
 import munit.FunSuite
 import scala.annotation.tailrec
+import scala.collection.immutable.Queue
 
+/**
+ * Tests that exercise a variety of solutions to some random questions from leetcode.com.
+ * The solution templates there are painfully derived from languages that most commonly use arrays
+ * and vectors, hence the prevalence of Array instead of the more Scala-idiomatic List.
+ */
 class LeetcodeSolutions extends FunSuite {
-  test("1480. Running Sum of 1d Array") {
-    // https://leetcode.com/problems/running-sum-of-1d-array/
-    def runningSum(nums: Array[Int]): Array[Int] = {
-        nums.foldLeft(List.empty[Int]){ case (acc, n) =>
-            (acc.headOption.getOrElse(0) + n) :: acc
-        }.reverse.toArray
-    }
-
-    assertEquals(runningSum(Array(1,2,3)).toList, List(1,3,6))
-    assertEquals(runningSum(Array(-3, 4, 101, 0)).toList, List(-3, 1, 102, 102))
-  }
-
   test("1. Two Sum") {
     // https://leetcode.com/problems/two-sum/
     @tailrec
@@ -51,6 +45,65 @@ class LeetcodeSolutions extends FunSuite {
     assertEquals(shuffle(Array(2,5,1,3,4,7), 3).toList, List(2,3,5,4,1,7))
     assertEquals(shuffle(Array(1,2,3,4,4,3,2,1), 4).toList, List(1,4,2,3,3,2,4,1))
     assertEquals(shuffle(Array(1,1,2,2), 2).toList, List(1,2,1,2))
+  }
+
+  test("1480. Running Sum of 1d Array") {
+    // https://leetcode.com/problems/running-sum-of-1d-array/
+    def runningSum(nums: Array[Int]): Array[Int] = {
+        nums.foldLeft(List.empty[Int]){ case (acc, n) =>
+            (acc.headOption.getOrElse(0) + n) :: acc
+        }.reverse.toArray
+    }
+
+    assertEquals(runningSum(Array(1,2,3)).toList, List(1,3,6))
+    assertEquals(runningSum(Array(-3, 4, 101, 0)).toList, List(-3, 1, 102, 102))
+  }
+
+  test("1700. Number of Students Unable to Eat Lunch") {
+    // https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
+
+    @tailrec
+    def loopR(
+      students: Queue[Int],
+      sandwiches: Array[Int],
+      idx: Int,
+      numZeroStuds: Int,
+      numOneStuds: Int
+    ): Int = {
+      if (idx >= sandwiches.size) students.size
+      else if (sandwiches(idx) == 0 && numZeroStuds == 0) students.size
+      else if (sandwiches(idx) == 1 && numOneStuds == 0) students.size
+      else {
+        val (current, qq) = students.dequeue
+        if (current == sandwiches(idx)) {
+          // We found a match! One less student with that preference
+          if (current == 0) loopR(qq, sandwiches, idx + 1, numZeroStuds - 1, numOneStuds)
+          else loopR(qq, sandwiches, idx + 1, numZeroStuds, numOneStuds - 1)
+        } else {
+          // No match, return the student to the back of the queue and leave the rest of the
+          // data alone for the next iteration.
+          loopR(qq :+ current, sandwiches, idx, numZeroStuds, numOneStuds)
+        }
+      }
+    }
+
+    def countStudents(students: Array[Int], sandwiches: Array[Int]): Int = {
+      // Count the number of students w/ each preference ahead of time to improve our ability to
+      // know when to stop the iteration. This allows us to avoid doing something like calling
+      // contains() on the queue for each iteration to see if there are any students left who will
+      // consume the current sandwich at the top of the "stack".
+      val grouped = students.groupBy(identity).map{ case (k,v) => (k, v.size)}
+      loopR(Queue(students: _*), sandwiches, 0, grouped.get(0).getOrElse(0), grouped.get(1).getOrElse(0))
+    }
+
+    // Constraints are that both input arrays are the same size, and must have between 1 and 100
+    // elements, and all elements must be either 0 or 1.
+    assertEquals(countStudents(Array(1,0), Array(0,1)), 0)
+    assertEquals(countStudents(Array(1,1,0), Array(0,0,0)), 2)
+    assertEquals(countStudents(Array(1,0,1,1), Array(0,1,0,1)), 2)  // ORDERING MATTERS!
+    assertEquals(countStudents(Array(1,0,1,1), Array(0,1,1,0)), 1)  // ORDERING MATTERS!
+    assertEquals(countStudents(Array(1,1,0,0), Array(0,1,0,1)), 0)
+    assertEquals(countStudents(Array(1,1,1,0,0,1), Array(1,0,0,0,1,1)), 3)
   }
 
   // test("1825. Finding MK Average") {
