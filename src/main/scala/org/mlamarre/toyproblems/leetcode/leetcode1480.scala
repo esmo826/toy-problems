c8635a474785388cce5b4315ac597a165b7ca3f6
mlamarre <mike.lamarre@gmail.com>
2021-06-21 21:52:40 -0700

    Refactor solutions and tests to their own packages/classes

---
+++ src/main/scala/org/mlamarre/toyproblems/leetcode/leetcode1480.scala
@@ -0,0 +1,13 @@
+package org.mlamarre.toyproblems.leetcode
+
+package leetcode1480 {
+
+  object Solution {
+    def runningSum(nums: Array[Int]): Array[Int] = {
+      nums
+        .foldLeft(List.empty[Int]) { case (acc, n) => (acc.headOption.getOrElse(0) + n) :: acc }
+        .reverse
+        .toArray
+    }
+  }
+}
---
+++ src/test/scala/org/mlamarre/toyproblems/leetcode/Leetcode1480Test.scala
@@ -0,0 +1,15 @@
+package org.mlamarre.toyproblems.leetcode
+
+import munit.FunSuite
+
+class Leetcode1480Test extends FunSuite {
+
+  // https://leetcode.com/problems/running-sum-of-1d-array/
+  test("1480. Running Sum of 1d Array") {
+    import leetcode1480.Solution._
+
+    assertEquals(runningSum(Array(1, 2, 3)).toList, List(1, 3, 6))
+    assertEquals(runningSum(Array(-3, 4, 101, 0)).toList, List(-3, 1, 102, 102))
+  }
+
+}
