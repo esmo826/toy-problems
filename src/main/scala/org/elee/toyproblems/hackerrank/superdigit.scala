43d04e2d1df734f8f14db8b02a6e1ae56b2950e5
Esmond Lee <Esmond_Lee@cable.comcast.com>
2022-02-21 15:55:08 -0800

    cleaned up superdigits and added more comments

--- src/main/scala/org/elee/toyproblems/hackerrank/superdigit.scala
+++ src/main/scala/org/elee/toyproblems/hackerrank/superdigit.scala
@@ -2,69 +2,45 @@ package org.elee.toyproblems.hackerrank
 import scala.io.StdIn.readLine
 
 // https://www.hackerrank.com/challenges/super-digit/problem
+/*
+We define super digit of an integer  using the following rules:
+If  has only  digit, then its super digit is .
+Otherwise, the super digit of  is equal to the super digit of the digit-sum of . Here, digit-sum of a number is defined as the sum of its digits.
+*/
 package superdigit {
 
   object Solution {
 
-    def createPString(n: String, k: Int): String = {
-        (0 until k).toList.foldLeft("")((acc, _) => acc + n )
+
+    def digitSumString(p: String): Long = {
+        (p.toList.map(x => Integer.parseInt(x.toString)).fold(0)(_+_)).toLong
     }
 
-    def extractDigits(n: Long, accum: List[Long]): List[Long] = {
-        n match {
-            case 0 => accum
-            case _ => n % 10L +: extractDigits(n / 10L, accum)
+    def digitSum(n: Long): Long = {
+        // extracts the individual digits of a long into a list
+        def extractDigits(n: Long, accum: List[Long]): List[Long] = {
+            n match {
+                case 0 => accum
+                // Seems like there should be a better way to do this.
+                case _ => n % 10L +: extractDigits(n / 10L, accum)
+            }
         }
-    }
-
-    def digitSum(p: String): String = {
-        p.toList.map(x => Integer.parseInt(x.toString)).fold(0)(_+_).toString
-    }
-
-    def digitSum2(n: Long): Long = {
         extractDigits(n, List.empty).fold(0L)(_+_)
     }
 
-    def superDigit(p: String, firstIter: Boolean): String = {
-
-        val pSize = p.size
-
-        if (firstIter)
-            superDigit(digitSum(p), false)
-        else {
-            pSize match {
-                case 0 => "0"
-                case 1 => p
-                case _ => superDigit(digitSum(p.dropRight(1)), false)
-            }
-        }
-    }
-
-    @scala.annotation.tailrec
-    def superDigit2(n: Long): Long = {
-        if (n < 10)
-            n
-        else superDigit2(digitSum2(n))
-    }
-
-    def superDigit3(k: String, n: Int): Long = {
-
-        val pReduced: Long = digitSum(k).toLong * n
-
-        if (pReduced < 10)
-            pReduced
-        else
-            superDigit3(digitSum2(pReduced).toString, 1)
+    def superDigit(k: String, n: Int): Long = {
+        // Reducing P to digitsum * n so we don't need to waste memory creating a giant string.
+        val pReduced: Long = digitSumString(k) * n
+        // If a number is less than 10, P size == 1.
+        if (pReduced < 10) pReduced
+        else superDigit(digitSum(pReduced).toString, 1)
     }
 
     def main(args: Array[String]): Unit = {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
         val input = readLine()
         val parsedInputs = input.split(" ")
-        //val pReduced = digitSum(parsedInputs(0).toLong).toInt * parsedInputs(1).toInt;
-        //val sd = superDigit(createPString(parsedInputs(0), parsedInputs(1).toInt), true)
-        //val sd = superDigit2(pReduced)
-        val sd = superDigit3(parsedInputs(0), parsedInputs(1).toInt)
+        val sd = superDigit(parsedInputs(0), parsedInputs(1).toInt)
         println(s"$sd")
 
     }
